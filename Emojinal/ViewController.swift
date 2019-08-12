13e90abfa964fa12dd68e521d52d6c001529ee04
Your Name <you@example.com>
2019-08-12 11:20:11 -0400

    Emojinal2

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -16,5 +16,14 @@ class ViewController: UIViewController {
     }
 
 
-}
 
+@IBAction func showMessage(sender: UIButton) {
+    
+    // Make a function where when an emoji is clicked it brings out a different message each time
+  
+ let alertController = UIAlertController( title: "Emojinal", message: "Feeling a bit mischievious, aren't we?", preferredStyle: UIAlertController.Style.alert)
+    
+alertController.addAction(UIAlertAction( title: "Emojinal", style: UIAlertAction.Style.default, handler : nil))
+    present(alertController, animated: false, completion : nil)
+}
+}
